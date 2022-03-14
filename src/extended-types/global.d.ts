5b9959da3defcc67c03766bbf45aa77e5e600148
Luiz Ribeiro <ltrindaderibeiro@gmail.com>
2022-03-13 21:55:56 -0300

    feat(config): add environment parsing

---
+++ src/extended-types/global.d.ts
@@ -0,0 +1,9 @@
+namespace NodeJS {
+  interface ProcessEnv {
+    NODE_ENV: string;
+    KAFKA_KEY: string;
+    KAFKA_SECRET: string;
+    KAFKA_BOOTSTRAP_SERVER: string;
+    TOPIC: string;
+  }
+}

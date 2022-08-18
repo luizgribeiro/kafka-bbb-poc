12a6a87eb9ecdc481a8903acb976a1e40f58c8b5
Luiz Ribeiro <ltrindaderibeiro@gmail.com>
2022-08-18 17:45:20 -0300

    remove typescript from project

--- src/extended-types/global.d.ts
+++
@@ -1,9 +0,0 @@
-namespace NodeJS {
-  interface ProcessEnv {
-    NODE_ENV: string;
-    KAFKA_KEY: string;
-    KAFKA_SECRET: string;
-    KAFKA_BOOTSTRAP_SERVER: string;
-    TOPIC: string;
-  }
-}
